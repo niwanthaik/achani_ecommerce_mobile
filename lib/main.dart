996fa3cafb01264fb02def85a4e396bffe255682
Niwantha <niwantha@octopuspos.com>
2025-08-02 23:01:38 +1000

    firebase integration success

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'bloc/counter_state.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
